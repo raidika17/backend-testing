1d9a377f39553e27b498b70bc9ecd3f8d3fd5f93
unknown <raidika03@gmail.com>
2024-08-30 02:56:10 +0700

    my backend app

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express, { Application, Response, Request, NextFunction } from "express";
+
+const app: Application = express();
+const port: Number = 4000;
+
+app.use("/user", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send([
+    {
+      id: 1,
+      name: "admin kantor",
+      username: "admin17",
+      password: "admin",
+      role: "admin",
+    },
+    {
+      id: 2,
+      name: "user kantor",
+      username: "user",
+      password: "user",
+      role: "user",
+    },
+  ]);
+});
+
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
